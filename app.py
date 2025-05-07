7450892e80c6bd47815013062b4a39495a408838
Hamgym <pipiyakas@gmail.com>
2025-05-07 22:19:42 +0800

    update

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI, Form, Request, UploadFile
 from fastapi.responses import FileResponse
+from models.rdb import *
+import boto3, uuid
 app = FastAPI()
-
-
-import boto3
 s3 = boto3.resource("s3")
 BUCKET_NAME = "hamgym"
 
@@ -12,11 +11,13 @@ BUCKET_NAME = "hamgym"
 async def get_index(request:Request):
   return FileResponse("./static/index.html", media_type="text/html")
 
-from models.rdb import *
+
 @app.post("/article")
 async def post_article(text:str=Form(), image:UploadFile=Form()):
   if image.size==0 or image.filename=="":
     return {"text": text, "imageURL": None}
+  rnd_name = uuid.uuid4().hex[:12]
+  image.filename = rnd_name
   s3.Bucket(BUCKET_NAME).put_object(Key=image.filename, Body=image.file)
   image_url = f"https://d2j1hqbdhu9zhx.cloudfront.net/{image.filename}"
   CRUD.create_article(text, image_url)
